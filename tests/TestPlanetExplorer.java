f4e21ee38d54041c82f0b6afacf677c1d908bd9e
somename <someemail>
2017-06-08 20:11:03 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -11,7 +11,8 @@ public class TestPlanetExplorer {
 	
 	@Test
 	public void test_Kretanje_Komanda_f(){
-	PlanetExplorer explorer=new PlanetExplorer(0,0,N);
+	PlanetExplorer explorer=new PlanetExplorer(100,100,"(5,5),(7,8)");
+	
 	String expected=explorer.executeCommand("f");
 	assertEquals("Not expected","1,0,N",expected);
 	
