dce23c3f4c8977398a10ae1847253b28bbca8772
somename <someemail>
2017-06-08 20:06:07 +0200

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -4,6 +4,8 @@
 // Student ID:
 
 public class PlanetExplorer {
+	private String f,l,r,b;
+	private int[]coordinates={0,0};
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -16,6 +18,7 @@ public class PlanetExplorer {
 	
 	public String executeCommand(String command){
 		
+		
 		/* The command string is composed of "f" (forward), "b" (backward), "l" (left) and "r" (right)
 		 * Example: 
 		 * The explorer is on a 100x100 grid at location (0, 0) and facing NORTH. 
@@ -25,7 +28,9 @@ public class PlanetExplorer {
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
-		
+		if(command==f){
+			coordinates[0]++;
+		}
 		
 		return null;
 	}
